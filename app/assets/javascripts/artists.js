bfc24aad10dbf0c14d3a0dd37d2b38a86932c7ec
theletterrsquared@gmail.com <theletterrsquared@gmail.com>
2016-03-01 18:28:43 -0800

    fixed carousel initialization problem

--- app/assets/javascripts/artists.js
+++ app/assets/javascripts/artists.js
@@ -1,8 +1,4 @@
 $( document ).ready(function () {
 	console.log("THIS ARTIST PAGE WORKS");
-	$(".owl-carousel").owlCarousel({
-	  navigation : true, // Show next and prev button
-      itemsDesktop : [1199,3],
-      itemsDesktopSmall : [979,3]
-	});
+
 });
--- app/assets/javascripts/galleries.js
+++ app/assets/javascripts/galleries.js
@@ -1,8 +1,5 @@
 $( document ).ready(function () {
 	console.log("THIS ARTIST PAGE WORKS");
-	$("#slides").owlCarousel({
-	  navigation : true, // Show next and prev button
-      itemsDesktop : [1199,3],
-      itemsDesktopSmall : [979,3]
-	});
+
 });
+
