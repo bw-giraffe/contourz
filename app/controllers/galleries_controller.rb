c2d2c9ee8c2512f855592bd6bd59b31f6025595c
theletterrsquared@gmail.com <theletterrsquared@gmail.com>
2016-03-01 18:31:48 -0800

    removed gratuitous logging for renditions and galleries

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -5,13 +5,11 @@ class GalleriesController < ApplicationController
   end
 
   def show
-    p "ID IS #{params[:id]}"
     @gallery = Gallery.find_by_id(params[:id])
     @photos = @gallery.photos
   end
 
   def renditions
-    @lastdrawing = Drawing.last
     @original = Photo.find_by_id(params[:id])
     @renditions = Drawing.where(photo_id: params[:id]).last(50)
     render :renditions #lol
