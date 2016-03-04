1ca7b50eb564766778d755b7ba28f217d57d785c
theletterrsquared@gmail.com <theletterrsquared@gmail.com>
2016-03-03 19:06:22 -0800

    fixing color select and thickness selection ui

--- app/assets/javascripts/drawings.js
+++ app/assets/javascripts/drawings.js
@@ -5,6 +5,11 @@ $( document ).ready(function () {
 		$(this).css("background", circFill);
 	});
 
+	$('.thick').each(function (e) {
+		height = $(this).data('height');
+		$(this).css("height", height);
+	});
+
 	ctx = document.getElementById('mCanvasBottom').getContext('2d');
 	ctx.strokeStyle = "#ff00ff";	
 	ctx.lineWidth = 5;
@@ -13,14 +18,14 @@ $( document ).ready(function () {
 			e = e.originalEvent;
 			ctx.beginPath();
 			x = e.changedTouches[0].pageX;
-			y = e.changedTouches[0].pageY-300;
+			y = e.changedTouches[0].pageY-325;
 			ctx.moveTo(x, y);
 		};
 		var move = function(e) {
 			e.preventDefault();
 			e = e.originalEvent;
 			x = e.changedTouches[0].pageX;
-			y = e.changedTouches[0].pageY-300;
+			y = e.changedTouches[0].pageY-320;
 			ctx.lineTo(x,y);
 			ctx.stroke();
 		};
@@ -31,6 +36,11 @@ $( document ).ready(function () {
 		$('#colors').bind('touchstart', function (e) {
     		color = $(e.target).data('fill');
     		ctx.strokeStyle = color;
+		});
+
+		$('.thick').bind('touchstart', function (e) {
+			stroke = $(e.target).data('stroke');
+			ctx.lineWidth = stroke;
 		});
 		
 });
